3edd2956606dc7109a6b870c49a5a11250fb06fc
Disha Patel <dishap1509@gmail.com>
2020-12-25 03:00:45 +0530

    feat: add end-points

--- server/env.js
+++ server/env.js
@@ -1,9 +1,10 @@
-require('dotenv').config()
+require('dotenv').config();
 exports.GOOGLE_APPLICATION_CREDENTIALS =
-  process.env.GOOGLE_APPLICATION_CREDENTIALS
+  process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
 exports.DATABASE_URL =
   process.env.DATABASE_URL ||
-  'https://ancora-imparo-polls-default-rtdb.firebaseio.com'
+  'https://ancora-imparo-polls-default-rtdb.firebaseio.com';
 exports.DATABASE_AUTH_VARIABLE_UID =
-  process.env.DATABASE_AUTH_VARIABLE_UID || 'my-service-worker'
+  process.env.DATABASE_AUTH_VARIABLE_UID || 'my-service-worker';
+exports.SERVER_PORT = process.env.SERVER_PORT || '4000';
