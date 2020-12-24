f38c859b5ca131012755c8178a27d0ffdbf1c2d9
kartikp36 <kartikp2002@gmail.com>
2020-12-25 04:01:29 +0530

    feat: basic-endpoints using axios

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function Home() {
-  return <h2>Polls</h2>;
+  const [polls, setPolls] = useState([]);
+  (async () => {
+    try {
+      const response = await axios.get(`http://localhost:4000/polls`);
+      const get_data = response.data;
+      setPolls(get_data);
+    } catch (err) {
+      console.log(err);
+    }
+  })();
+
+  return (
+    <center>
+      <h2>Polls</h2>
+      <pre>{JSON.stringify(polls)}</pre>
+    </center>
+  );
 }
