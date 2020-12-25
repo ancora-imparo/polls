f8949e681a4306c6d22635b71d6bf0419e7550fa
Disha Patel <dishap1509@gmail.com>
2020-12-25 23:59:14 +0530

    fix: environment variable

--- server/env.js
+++ server/env.js
@@ -7,4 +7,4 @@ exports.DATABASE_URL =
   'https://ancora-imparo-polls-default-rtdb.firebaseio.com';
 exports.DATABASE_AUTH_VARIABLE_UID =
   process.env.DATABASE_AUTH_VARIABLE_UID || 'my-service-worker';
-exports.SERVER_PORT = process.env.SERVER_PORT || '4000';
+exports.SERVER_PORT = process.env.PORT || process.env.SERVER_PORT || '4000';
