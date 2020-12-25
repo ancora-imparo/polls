f685dbd254e5fbd2d5cff4417815318616e61d38
Disha Patel <dishap1509@gmail.com>
2020-12-26 02:45:22 +0530

    feat: create/update poll with uuid

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const admin = require('firebase-admin');
 const express = require('express');
 const app = express();
+app.use(express.json());
+const Joi = require('joi');
+const { v4: uuidv4 } = require('uuid');
+const _ = require('lodash');
 
 const env = require('./env');
 const store = require('./store');
@@ -20,17 +24,27 @@ app.get('/polls', async (req, res) => {
   const polls = await store.readFromRef();
   res.set('Access-Control-Allow-Origin', '*').status(200).send(polls);
 });
-const polls = [
-  {
-    pollId: '1',
-    question: 'Sun rises from East?',
-    option1: { id: 'xfch', text: 'true' },
-    option2: { id: 'cscf', text: 'false' },
-  },
-];
+const validatPoll = (polls) => {
+  const schema = Joi.object({
+    question: Joi.string().required(),
+    option1: Joi.string().required(),
+    option2: Joi.string().required(),
+    pollId: Joi.string(),
+  });
+  return schema.validate(polls);
+};
 app.post('/polls', async (req, res) => {
-  await store.writeToRef(polls);
-  res.status(200).send();
+  const { error } = validatPoll(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const polls = {
+    pollId: req.body.pollId || uuidv4(),
+    question: req.body.question,
+    option1: { id: uuidv4(), text: req.body.option1 },
+    option2: { id: uuidv4(), text: req.body.option2 },
+  };
+
+  await store.writeToRef(polls, `/${polls.pollId}`);
+  res.status(200).send(polls.pollId);
 });
 
 const port = env.SERVER_PORT;
