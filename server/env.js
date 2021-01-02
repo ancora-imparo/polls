3656f4caabd5811fa6d23409ab5f2b525bc13448
Disha Patel <dishap1509@gmail.com>
2021-01-03 01:03:16 +0530

    chore: pass client sdk values

--- server/env.js
+++ server/env.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
-exports.GOOGLE_APPLICATION_CREDENTIALS =
+exports.GOOGLE_APPLICATION_CREDENTIALS_SERVER =
   process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
 exports.DATABASE_URL =
   process.env.DATABASE_URL ||
   'https://ancora-imparo-polls-default-rtdb.firebaseio.com';
-exports.DATABASE_AUTH_VARIABLE_UID =
-  process.env.DATABASE_AUTH_VARIABLE_UID || 'my-service-worker';
+exports.DATABASE_AUTH_VARIABLE_UID_SERVER =
+  process.env.DATABASE_AUTH_VARIABLE_UID_SERVER || 'my-service-worker';
 exports.SERVER_PORT = process.env.PORT || process.env.SERVER_PORT || '4000';
+
+exports.GOOGLE_APPLICATION_CREDENTIALS_CLIENT =
+  process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+exports.DATABASE_AUTH_VARIABLE_UID_CLIENT =
+  process.env.DATABASE_AUTH_VARIABLE_UID_CLIENT || 'my-service-worker';
