5701609db6974085a7ed7cd209581463f3cd8376
kartikp36 <kartikp2002@gmail.com>
2021-01-30 16:06:19 +0530

    fix: sdk request in server

--- server/env.js
+++ server/env.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 exports.GOOGLE_APPLICATION_CREDENTIALS_SERVER =
   process.env.GOOGLE_APPLICATION_CREDENTIALS_SERVER;
-
 exports.DATABASE_URL =
   process.env.DATABASE_URL ||
   'https://ancora-imparo-polls-default-rtdb.firebaseio.com';
@@ -9,8 +8,10 @@ exports.DATABASE_AUTH_VARIABLE_UID_SERVER =
   process.env.DATABASE_AUTH_VARIABLE_UID_SERVER || 'my-service-worker';
 exports.SERVER_PORT = process.env.PORT || process.env.SERVER_PORT || '4000';
 
-exports.GOOGLE_APPLICATION_CREDENTIALS_CLIENT =
-  process.env.GOOGLE_APPLICATION_CREDENTIALS_CLIENT;
-
-exports.DATABASE_AUTH_VARIABLE_UID_CLIENT =
-  process.env.DATABASE_AUTH_VARIABLE_UID_CLIENT || 'my-service-worker';
+exports.AUTH_DOMAIN =
+  process.env.AUTH_DOMAIN || 'ancora-imparo-polls.firebaseapp.com';
+exports.API_KEY = process.env.API_KEY;
+exports.PROJECT_ID = process.env.PROJECT_ID || 'ancora-imparo-polls';
+exports.SENDER_ID = process.env.SENDER_ID;
+exports.APP_ID = process.env.APP_ID;
+exports.MEASUREMENT_ID = process.env.MEASUREMENT_ID;
