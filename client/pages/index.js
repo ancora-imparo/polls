c02794a3c9caa76c22cc79784eaae6975df55e62
kartikp36 <kartikp2002@gmail.com>
2021-01-30 16:08:35 +0530

    feat: init and read firebase database

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,33 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-import initializeFirebase from './firebase';
+import 'firebase/database';
+import firebase from 'firebase/app';
 
 export default function Home() {
   const [polls, setPolls] = useState([]);
+
   useEffect(async () => {
     try {
-      const response = await axios.get(
-        `https://ancora-imparo-polls-api.herokuapp.com/polls`
-      );
-      const getData = response.data;
-      setPolls(getData);
-    } catch (err) {
-      console.error('error:', err);
-    }
-    try {
       const response = await axios.get('http://localhost:4000/sdk');
-      const endpoints = response.data;
-      initializeFirebase(endpoints);
+      const firebaseConfig = response.data;
+
+      await firebase.initializeApp(firebaseConfig);
+      readFromRef();
     } catch (err) {
       console.error('error:', err);
     }
   }, []);
 
+  const readFromRef = async (refrencePath = '/') => {
+    const db = firebase.database();
+    const ref = db.ref(refrencePath);
+    const snapshot = await ref.once('value');
+    setPolls(snapshot.val());
+  };
   return (
-    <center>
+    <div>
       <h2>Polls</h2>
       <pre>{JSON.stringify(polls, null, 2)}</pre>
-    </center>
+    </div>
   );
 }
