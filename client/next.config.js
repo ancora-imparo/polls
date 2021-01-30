f96ae315c0b0fb83b0feaf35fa3303ffe01b28b1
kartikp36 <kartikp2002@gmail.com>
2021-01-31 00:14:03 +0530

    fix: npm run local script and apiBase

---
+++ client/next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    API_BASE: process.env.API_BASE,
+  },
+};
--- client/pages/index.js
+++ client/pages/index.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 import { initializeFirebase, readFromRef } from './firebase';
 export default function Home() {
   const [polls, setPolls] = useState({});
+  const apiBase =
+    process.env.API_BASE || `https://ancora-imparo-polls-api.herokuapp.com`;
   useEffect(async () => {
     try {
-      const response = await axios.get(
-        `https://ancora-imparo-polls-api.herokuapp.com/polls`
-      );
+      const response = await axios.get(`${apiBase}/sdk`);
       const firebaseConfig = response.data;
       await initializeFirebase(firebaseConfig);
       const data = await readFromRef();
